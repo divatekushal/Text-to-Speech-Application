a46bb25cbb07d26d0752b1a2342535932cd13081
Kushal Divate <144881705+divatekushal@users.noreply.github.com>
2024-04-24 01:57:25 +0530

    Update code-TTS.py

--- code-TTS.py
+++ code-TTS.py
@@ -1,81 +1,97 @@
-import tkinter as tk
-from tkinter import ttk
 import pyttsx3
+import customtkinter
+import tkinter as tk
+
+
+customtkinter.set_appearance_mode("System")  
+customtkinter.set_default_color_theme("blue")  
 
 class TextToSpeechApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Text-to-Speech Application")
-        self.root.configure(bg="lightblue")
+        frame = customtkinter.CTkFrame(master=root, width=340, height=500, corner_radius=15)
+        frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         self.engine = pyttsx3.init()
 
-        self.heading_label = tk.Label(root, text="Text to Speech", font=("Arial", 14, "bold"), fg="lightblue", bg="blue")
-        self.heading_label.grid(row=0, column=0, columnspan=3, padx=10, pady=5, sticky="nsew")
+        self.heading_label = customtkinter.CTkLabel(master=frame, text="Text to Speech", font=("Century Gothic", 20, ), text_color="lightblue")
+        self.heading_label.place(relx=0.5, y=15, anchor="center")
 
-        self.enter_text_label = tk.Label(root, text="Enter your text:", bg="lightblue",fg="blue")
-        self.enter_text_label.grid(row=1, column=0, padx=10, pady=0, sticky="w")
+        self.enter_text_label = customtkinter.CTkLabel(master=frame, text="Enter your text:")
+        self.enter_text_label.place(x=10, y=50)
 
-        self.text_input = tk.Text(root, height=10, width=40,fg="blue")
-        self.text_input.grid(row=2, column=0, columnspan=3, padx=10, pady=5)
+        self.text_input = customtkinter.CTkTextbox(master=frame, height=150, width=300)
+        self.text_input.place(x=10, y=80)
 
-        self.language_label = tk.Label(root, text="Select Language:",fg="blue", bg="lightblue")
-        self.language_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
-        self.language_var = tk.StringVar()
-        self.language_dropdown = ttk.Combobox(root, textvariable=self.language_var,)
-        self.language_dropdown.grid(row=3, column=1, padx=10, pady=5, sticky="w")
+        self.language_label = customtkinter.CTkLabel(master=frame, text="Select Language:")
+        self.language_label.place(x=10, y=250)
+        self.language_var = customtkinter.StringVar(master=frame)
+        self.language_dropdown = customtkinter.CTkComboBox(master=frame)
+        self.language_dropdown.place(x=130, y=250)
 
-        self.voice_label = tk.Label(root, text="Select Voice:",fg="blue", bg="lightblue")
-        self.voice_label.grid(row=4, column=0, padx=10, pady=5, sticky="w")
-        self.voice_var = tk.StringVar()
-        self.voice_dropdown = ttk.Combobox(root, textvariable=self.voice_var,)
-        self.voice_dropdown.grid(row=4, column=1, padx=10, pady=5, sticky="w")
+        self.voice_label = customtkinter.CTkLabel(master=frame, text="Select Voice:")
+        self.voice_label.place(x=10, y=280)
+        self.voice_var = customtkinter.StringVar(master=frame)
+        self.voice_dropdown = customtkinter.CTkComboBox(master=frame)
+        self.voice_dropdown.place(x=130, y=280)
 
         self.update_languages()
 
-        self.rate_label = tk.Label(root, text="Rate:",fg="blue", bg="lightblue")
-        self.rate_label.grid(row=5, column=0, padx=10, pady=5, sticky="w")
-        self.rate_slider = tk.Scale(root, from_=50, to=200, orient="horizontal",bg="lightblue",fg="blue")
-        self.rate_slider.grid(row=5, column=1, padx=10, pady=5, sticky="w")
+        self.rate_label = customtkinter.CTkLabel(master=frame, text="Rate:")
+        self.rate_label.place(x=10, y=310)
+        self.rate_slider = customtkinter.CTkSlider(master=frame, from_=50, to=200)
+        self.rate_slider.place(x=130, y=310)
 
-        self.pitch_label = tk.Label(root, text="Pitch:", bg="lightblue",fg="blue")
-        self.pitch_label.grid(row=6, column=0, padx=10, pady=5, sticky="w")
-        self.pitch_slider = tk.Scale(root, from_=0, to=20, orient="horizontal",bg="lightblue",fg="blue")
-        self.pitch_slider.grid(row=6, column=1, padx=10, pady=5, sticky="w")
+        self.pitch_label = customtkinter.CTkLabel(master=frame, text="Pitch:")
+        self.pitch_label.place(x=10, y=340)
+        self.pitch_slider = customtkinter.CTkSlider(master=frame, from_=0, to=20)
+        self.pitch_slider.place(x=130, y=340)
 
-        self.volume_label = tk.Label(root, text="Volume:",fg="blue", bg="lightblue")
-        self.volume_label.grid(row=7, column=0, padx=10, pady=5, sticky="w")
-        self.volume_slider = tk.Scale(root, from_=0, to=100, orient="horizontal",bg="lightblue",fg="blue")
-        self.volume_slider.grid(row=7, column=1, padx=10, pady=5, sticky="w")
+        self.volume_label = customtkinter.CTkLabel(master=frame, text="Volume:")
+        self.volume_label.place(x=10, y=370)
+        self.volume_slider = customtkinter.CTkSlider(master=frame, from_=0, to=100)
+        self.volume_slider.place(x=130, y=370)
 
-        self.play_button = tk.Button(root, text="Play", command=self.play_text,bg="blue",fg="lightblue")
-        self.play_button.grid(row=8, column=0, padx=10, pady=5, sticky="w")
+        self.play_button = customtkinter.CTkButton(master=frame, text="Play", command=self.play_text,width=100)
+        self.play_button.place(x=10, y=410)
 
-        self.pause_button = tk.Button(root, text="Pause", command=self.pause_text,bg="blue",fg="lightblue")
-        self.pause_button.grid(row=8, column=1, padx=10, pady=5, sticky="w")
+        self.pause_button = customtkinter.CTkButton(master=frame, text="Pause", command=self.pause_text,width=100)
+        self.pause_button.place(x=120,y=410)
 
-        self.stop_button = tk.Button(root, text="Stop", command=self.stop_text,bg="blue",fg="lightblue")
-        self.stop_button.grid(row=8, column=2, padx=10, pady=5, sticky="w")
+        self.stop_button = customtkinter.CTkButton(master=frame, text="Stop", command=self.stop_text,width=100)
+        self.stop_button.place(x=230, y=410)
 
-        self.save_button = tk.Button(root, text="Save Audio", command=self.save_audio,bg="blue",fg="lightblue")
-        self.save_button.grid(row=9, column=0, columnspan=3, padx=10, pady=5, sticky="nsew")
+        self.save_button = customtkinter.CTkButton(master=frame, text="Save Audio", command=self.save_audio)
+        self.save_button.place(relx=0.5, y=470,anchor="center")
 
     def update_languages(self):
         self.language_dropdown['values'] = ["English"]
+        self.language_dropdown.set("English")  
+
         self.update_voices()
 
     def update_voices(self, *args):
         voices = self.engine.getProperty('voices')
         voice_names = [voice.name for voice in voices]
-        self.voice_dropdown['values'] = voice_names
+        self.voice_dropdown['values'] = ["Male"]
+        self.voice_dropdown.set("Male")
+        
+        if not voice_names: 
+            self.voice_dropdown['values'] = ["Male"]
+            self.voice_dropdown.set("Male")
+        else:
+            self.voice_dropdown['values'] = voice_names
+            default_male_voice = [voice.name for voice in voices if 'male' in voice.name.lower() and 'english' in voice.name.lower()]
+            default_female_voice = [voice.name for voice in voices if 'female' in voice.name.lower() and 'english' in voice.name.lower()]
+            
+            if default_male_voice:
+                self.voice_dropdown.set(default_male_voice[0])
+            elif default_female_voice:
+                self.voice_dropdown.set(default_female_voice[0])
 
-        default_male_voice = [voice.name for voice in voices if 'male' in voice.name.lower() and 'english' in voice.name.lower()]
-        default_female_voice = [voice.name for voice in voices if 'female' in voice.name.lower() and 'english' in voice.name.lower()]
 
-        if default_male_voice:
-            self.voice_dropdown.set(default_male_voice[0])
-        elif default_female_voice:
-            self.voice_dropdown.set(default_female_voice[0])
+                    
 
     def play_text(self):
         text = self.text_input.get("1.0", "end-1c")
@@ -118,6 +134,9 @@ class TextToSpeechApp:
         print(f"Audio file saved as {file_name}")
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = TextToSpeechApp(root)
-    root.mainloop()
+    app = customtkinter.CTk()
+    app.title('Text to Speech App')
+    app.geometry("400x600")
+    TextToSpeechApp(app)
+    
+    app.mainloop()
